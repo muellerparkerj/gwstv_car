c43c2ea8a5978b6249d338edc613fbfd2a31cd4c
parker@ellaton.com <parker@ellaton.com>
2021-03-06 01:05:01 -0500

    Version 1.0

---
+++ gwstv_car/CarCommand.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+
+namespace gwstv_car
+{
+    public class CarCommand : BaseScript
+    {
+        public CarCommand()
+        {
+            EventHandlers["onClientResourceStart"] += new Action<string>(OnClientResourceStart);
+        }
+
+        private void OnClientResourceStart(string resourceName)
+        {
+            if (GetCurrentResourceName() != resourceName) return;
+
+            RegisterCommand("car", new Action<int, List<object>, string>(async (source, args, raw) =>
+            {
+                var model = "dominator";
+                if (args.Count > 0)
+                {
+                    model = args[0].ToString();
+                }
+
+                var hash = (uint) GetHashKey(model);
+                if (!IsModelInCdimage(hash) || !IsModelAVehicle(hash))
+                {
+                    TriggerEvent("chat:addMessage", new
+                    {
+                        color = new[] { 255, 0, 0 },
+                        args = new[] { "[GWSTV_Car]", $"The model you're trying to spawn, not possible bud. Sorry not sorry." }
+                    });
+                    return;
+                }
+
+                var vehicle = await World.CreateVehicle(model, Game.PlayerPed.Position, Game.PlayerPed.Heading);
+                
+                Game.PlayerPed.SetIntoVehicle(vehicle, VehicleSeat.Driver);
+
+                TriggerEvent("chat:addMessage", new
+                {
+                    color = new[] {255, 0, 0},
+                    args = new[] { "[GWSTV_Car]", $"Spawned ^*{model}!" }
+                });
+            }), false);
+        }
+    }
+}
